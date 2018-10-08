63fee1671facc96daa3ea46c06a95815ef382364
Amauri Junior <amauri.junior@gmail.com>
2018-10-08 13:17:32 -0300

    Page 154
    Page 154

---
+++ #06-Functions/vsearch.py
@@ -0,0 +1,8 @@
+
+def search4vowels():
+    """Display any vowels found in an asked-for word."""
+    vowels = set('aeiou')
+    word = input('Provide a word to search for vowels: ')
+    found = vowels.intersection(set(word))
+    for vowel in found:
+            print(vowel)
