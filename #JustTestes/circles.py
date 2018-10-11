dfdb26d063505174b2278619b5d519813426274c
zorted <42916353+zorted@users.noreply.github.com>
2018-10-11 18:19:04 -0300

    Functions_Tests
    Functions_Tests

---
+++ #JustTestes/circles.py
@@ -0,0 +1,10 @@
+from math import pi
+
+
+def circle_area(r):
+    if type(r) not in [int, float]:
+        raise TypeError("The radius must be a non-negative real number.")
+
+    if r < 0:
+        raise ValueError("The radius cannot ve negative.")
+    return pi*(r**2)
