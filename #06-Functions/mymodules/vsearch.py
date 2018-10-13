1fac53457baf68bd5f7b0eccf98f6468a068ce3e
zorted <42916353+zorted@users.noreply.github.com>
2018-10-13 17:56:25 -0300

    Page 183 - Setuptools (Distribution Package)
    SetupToold - Tool used to install packages on main sites so these packages can be "called" from other parts of the project.
    from page 178 to 183

--- #06-Functions/mymodules/vsearch.py
+++ #06-Functions/mymodules/vsearch.py
@@ -1,12 +1,10 @@
 
-def search4vowels(phrase:str) -> set:
+def search4vowels(phrase: str) -> set:
     """Return any vowels found in a supplied word."""
-    vowels = set('aeiou')    
+    vowels = set('aeiou')
     return vowels.intersection(set(phrase))
 
 
-def search4letters(phrase: str, letters: str) -> set:
+def search4letters(phrase: str, letters: str = 'aeiou') -> set:
     """Return a set of the 'letters' found in 'phrase'."""
     return set(letters).intersection(set(phrase))
-
-
