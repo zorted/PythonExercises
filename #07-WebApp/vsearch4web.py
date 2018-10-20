4eb527a6971e83cdfaff3e5f3f821029b6179dae
zorted <42916353+zorted@users.noreply.github.com>
2018-10-20 16:52:58 -0300

    Page 237 - Final Webapp
    Page 237 - Final Webapp

--- #07-WebApp/vsearch4web.py
+++ #07-WebApp/vsearch4web.py
@@ -1,22 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from vsearch import search4letters
 
 app = Flask(__name__)
 
 
+@app.route('/search4', methods=['POST'])
+def do_search() -> 'html':
+    phrase = request.form['phrase']
+    letters = request.form['letters']
+    title = 'Here are ypir results: '
+    results = str(search4letters(phrase, letters))
+    return render_template('results.html',
+                           the_phrase=phrase,
+                           the_letters=letters,
+                           the_title=title,
+                           the_results=results,)
+
+
 @app.route('/')
-def hello() -> str:
-    return 'Hello world from Flask!'
-
-
-@app.route('/search4')
-def do_search() -> str:
-    return str(search4letters('Life, the universe and everything', 'eiru,!'))
-
-
 @app.route('/entry')
 def entry_page() -> 'html':
-    return render_template('entry.html', the_title='Welcome to search4letters on the web!')
+    return render_template('entry.html',
+                           the_title='Welcome to search4letters on the web!')
 
 
-app.run()
+app.run(debug=True)
