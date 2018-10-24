550fd7d9339b50ca4fcf2afd77e39f557b389c71
Amauri Junior <amauri.junior@gmail.com>
2018-10-24 13:34:43 -0300

    Page 254
    Page 254

--- #07-WebApp/vsearch4web.py
+++ #07-WebApp/vsearch4web.py
@@ -3,6 +3,11 @@ from vsearch import search4letters
 
 app = Flask(__name__)
 
+def log_request(req: 'flask_request', res: str) -> None:
+    with open('vsearch.log','a') as log:
+        print(req, res, file=log)
+
+
 
 @app.route('/search4', methods=['POST'])
 def do_search() -> 'html':
@@ -10,6 +15,7 @@ def do_search() -> 'html':
     letters = request.form['letters']
     title = 'Here are ypir results: '
     results = str(search4letters(phrase, letters))
+    log_request(request, results)
     return render_template('results.html',
                            the_phrase=phrase,
                            the_letters=letters,
