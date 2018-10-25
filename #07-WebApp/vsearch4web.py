d59c687ab664552d71c3a5a3e01dc840676bee5b
Amauri Junior <amauri.junior@gmail.com>
2018-10-25 13:30:12 -0300

    Page 266
    Page 266

--- #07-WebApp/vsearch4web.py
+++ #07-WebApp/vsearch4web.py
@@ -1,12 +1,11 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, escape
 from vsearch import search4letters
 
 app = Flask(__name__)
 
 def log_request(req: 'flask_request', res: str) -> None:
     with open('vsearch.log','a') as log:
-        print(req, res, file=log)
-
+        print(req.form, req.remote_addr, req.user_agent, res, file=log, sep='|')
 
 
 @app.route('/search4', methods=['POST'])
@@ -28,6 +27,13 @@ def do_search() -> 'html':
 def entry_page() -> 'html':
     return render_template('entry.html',
                            the_title='Welcome to search4letters on the web!')
+
+
+@app.route('/viewlog')
+def view_the_log() -> str:
+        with open('vsearch.log') as log:
+                contents = log.read()
+        return escape(contents)
 
 
 if __name__ == '__main__':
