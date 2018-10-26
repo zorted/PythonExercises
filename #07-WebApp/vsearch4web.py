353481cedd55dbfd55a700ce61c9789c18b10a0b
Amauri Junior <amauri.junior@gmail.com>
2018-10-26 15:16:35 -0300

    Page 275
    Page 275

--- #07-WebApp/vsearch4web.py
+++ #07-WebApp/vsearch4web.py
@@ -31,9 +31,13 @@ def entry_page() -> 'html':
 
 @app.route('/viewlog')
 def view_the_log() -> str:
+        contents = []
         with open('vsearch.log') as log:
-                contents = log.read()
-        return escape(contents)
+                for line in log:
+                        contents.append([])
+                        for item in line.split('|'):
+                                contents[-1].append(escape(item))                                                
+        return str(contents)
 
 
 if __name__ == '__main__':
